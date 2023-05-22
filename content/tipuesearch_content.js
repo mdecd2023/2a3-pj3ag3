cf277f0886009062b98bcefc6f71b25f1179cbb8
41023142 <41023142@gm.nfu.edu.tw>
2023-05-22 10:17:56 +0800

    edit

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site 41023142 \n', 'tags': '', 'url': 'About.html'}]};
-=======
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': '41039138 圖家風', 'tags': '', 'url': 'About.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:41023131(組長) \n \xa0 \xa0 \xa0 \xa041023101 \n \xa0 \xa0 \xa0 \xa041023108 \n \xa0 \xa0 \xa0 \xa041023110 \n \xa0 \xa0 \xa0 \xa041023130 \n \xa0 \xa0 \xa0 \xa041023141 \n \xa0 \xa0 \xa0 \xa041023142 \n \xa0 \xa0 \xa0 \xa0 41039138', 'tags': '', 'url': 'About.html'}]};
->>>>>>> 4b49f73fb2eebe091398e4c5595dd0623fa4246c
->>>>>>> 7c5fffa76f5fd36e6e1c1e7b1081c434b21e470b
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n 組員:41023131(組長) \n \xa0 \xa0 \xa0 \xa041023101 \n \xa0 \xa0 \xa0 \xa041023108 \n \xa0 \xa0 \xa0 \xa041023110 \n \xa0 \xa0 \xa0 \xa041023130 \n \xa0 \xa0 \xa0 \xa041023141 \n \xa0 \xa0 \xa0 \xa041023142 \n \xa0 \xa0 \xa0 \xa0 41039138', 'tags': '', 'url': 'About.html'}]};
->>>>>>> 38885bbac8c1863b61b6c914aed96467ab9e1aea
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲網址: https://github.com/mdecd2023/2a3-pj3ag3 \n \n \xa0 \xa0 \xa0 \xa041023101 \n \xa0 \xa0 \xa0 \xa041023108 \n \xa0 \xa0 \xa0 \xa041023110 \n \xa0 \xa0 \xa0 \xa041023130 \n \xa0 \xa0 \xa0 \xa041023141 \n \xa0 \xa0 \xa0 \xa041023142 \n \xa0 \xa0 \xa0 \xa0 41039138 \n', 'tags': '', 'url': 'About.html'}]};
