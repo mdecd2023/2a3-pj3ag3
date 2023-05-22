388b0bad4341b8a1229b2c8225fddfd9890de66f
41023142 <41023142@gm.nfu.edu.tw>
2023-05-22 09:47:45 +0800

    410231421

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site 41023142 \n', 'tags': '', 'url': 'About.html'}]};
