4b49f73fb2eebe091398e4c5595dd0623fa4246c
41023101 <yitingsailer@gmail.com>
2023-05-22 09:48:37 +0800

    edit

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '41039138\xa0', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:41023131(組長) \n \xa0 \xa0 \xa0 \xa041023101 \n \xa0 \xa0 \xa0 \xa041023108 \n \xa0 \xa0 \xa0 \xa041023110 \n \xa0 \xa0 \xa0 \xa041023130 \n \xa0 \xa0 \xa0 \xa041023141 \n \xa0 \xa0 \xa0 \xa041023142 \n \xa0 \xa0 \xa0 \xa0 41039138', 'tags': '', 'url': 'About.html'}]};
