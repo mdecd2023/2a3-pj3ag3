38885bbac8c1863b61b6c914aed96467ab9e1aea
41023101 <yitingsailer@gmail.com>
2023-05-22 10:02:35 +0800

    edit

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': '41039138 圖家風', 'tags': '', 'url': 'About.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:41023131(組長) \n \xa0 \xa0 \xa0 \xa041023101 \n \xa0 \xa0 \xa0 \xa041023108 \n \xa0 \xa0 \xa0 \xa041023110 \n \xa0 \xa0 \xa0 \xa041023130 \n \xa0 \xa0 \xa0 \xa041023141 \n \xa0 \xa0 \xa0 \xa041023142 \n \xa0 \xa0 \xa0 \xa0 41039138', 'tags': '', 'url': 'About.html'}]};
->>>>>>> 4b49f73fb2eebe091398e4c5595dd0623fa4246c
+var tipuesearch = {"pages": [{'title': 'About', 'text': '\n 組員:41023131(組長) \n \xa0 \xa0 \xa0 \xa041023101 \n \xa0 \xa0 \xa0 \xa041023108 \n \xa0 \xa0 \xa0 \xa041023110 \n \xa0 \xa0 \xa0 \xa041023130 \n \xa0 \xa0 \xa0 \xa041023141 \n \xa0 \xa0 \xa0 \xa041023142 \n \xa0 \xa0 \xa0 \xa0 41039138', 'tags': '', 'url': 'About.html'}]};
