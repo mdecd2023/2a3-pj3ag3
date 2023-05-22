ed715ab9050aa6d21c57857a5fbbf57d6e5eac2b
GUJIATENG <41039138@gm.nfu.edu.tw>
2023-05-22 09:31:25 +0800

    41039138

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '41039138 凃家豐  GUJIAFENG', 'tags': '', 'url': 'About.html'}]};
