988c8e352fe88dc01d3d1e59a923ebf78b84f70d
GUJIAFENG <113663778+GUJIAFENG@users.noreply.github.com>
2023-06-15 18:37:14 +0800

    Add files via upload

---
+++ downloads/pj3/zmq_ipv6_bubbleRob1.py
@@ -0,0 +1,46 @@
+# pip install pyzmq cbor keyboard
+#from zmqRemoteApi import RemoteAPIClient
+from zmqRemoteApi_IPv6 import RemoteAPIClient
+import keyboard
+client = RemoteAPIClient('192.168.1.114', 23000)
+ 
+print('Program started')
+sim = client.getObject('sim')
+sim.startSimulation()
+print('Simulation started')
+ 
+car = 4
+def setBubbleRobVelocity(leftfrontWheelVelocity, rightfrontWheelVelocity,leftbackWheelVelocity,rightbackWheelVelocity):
+    leftfrontMotor = sim.getObject('/car6/3')
+    rightfrontMotor = sim.getObject('/car6/2')
+    leftbackMotor = sim.getObject('/car6/4')
+    rightbackMotor = sim.getObject('/car6/1')
+     
+     
+    sim.setJointTargetVelocity(leftfrontMotor, leftfrontWheelVelocity)
+    sim.setJointTargetVelocity(rightfrontMotor, rightfrontWheelVelocity) 
+    sim.setJointTargetVelocity(leftbackMotor, leftbackWheelVelocity)
+    sim.setJointTargetVelocity(rightbackMotor, rightbackWheelVelocity)
+ 
+'''
+# Example usage 1:
+setBubbleRobVelocity(1.0, 1.0)
+time.sleep(2)
+setBubbleRobVelocity(0.0, 0.0)
+'''
+# use keyborad to move BubbleRob
+v  = 10.0
+while True:
+    if keyboard.is_pressed('w'):
+        setBubbleRobVelocity(v, v,v, v)
+    elif keyboard.is_pressed('s'):
+        setBubbleRobVelocity(-v, -v,-v, -v)
+    elif keyboard.is_pressed('a'):
+        setBubbleRobVelocity(-v, v,-v, v)
+    elif keyboard.is_pressed('d'):
+        setBubbleRobVelocity(v, -v,v, -v)
+    elif keyboard.is_pressed('q'):
+        # stop simulation
+        sim.stopSimulation()
+    else:
+        setBubbleRobVelocity(0.0, 0.0,0.0, 0.0)
